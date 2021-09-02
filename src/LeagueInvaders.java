155820d65a5d66c611f796a72e756f5b1ce73f55
Aidan <Aidan@DESKTOP-3GBAJ73.lan>
2021-09-01 19:59:11 -0700

    yeyeye

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,26 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
 
 public class LeagueInvaders {
+	public static void main(String[] args) {
+new LeagueInvaders().setup();
+	}
+JFrame frame;
+public static final int WIDTH = 500;
+public static final int HEIGHT = 800;
+LeagueInvaders(){
+	frame = new JFrame();
+}
+void setup() {
+	frame.setVisible(true);
+	frame.setSize(WIDTH, HEIGHT);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+public class GamePanel extends JPanel {
+	@Override
+	public void paintComponent(Graphics g){
 
+	}
+}
 }
