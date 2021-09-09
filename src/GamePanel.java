fd229b325a941f625350b435d55984f127d599e8
Aidan <Aidan@Aidan-GameDesktop.lan>
2021-09-08 20:01:51 -0700

    I worked on League Invaders

---
+++ src/GamePanel.java
@@ -0,0 +1,18 @@
+import java.awt.Font;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+		    final int MENU = 0;
+		    final int GAME = 1;
+		    final int END = 2;
+	    int currentState = MENU;
+		public void paintComponent(Graphics g){
+			//yguyt
+			Font titleFont = new Font("Arial", Font.PLAIN, 48);
+			titleFont = new Font("Arial", Font.PLAIN, 48
+					);
+		}
+
+	}
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,26 +1,33 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import java.awt.Font;
 import java.awt.Graphics;
 
 public class LeagueInvaders {
 	public static void main(String[] args) {
 new LeagueInvaders().setup();
+
 	}
+	
 JFrame frame;
+
 public static final int WIDTH = 500;
 public static final int HEIGHT = 800;
 LeagueInvaders(){
 	frame = new JFrame();
 }
 void setup() {
+	//astfe
+
+    
+	GamePanel panel = new GamePanel();
+	frame.add(panel);
 	frame.setVisible(true);
 	frame.setSize(WIDTH, HEIGHT);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 }
-public class GamePanel extends JPanel {
-	@Override
-	public void paintComponent(Graphics g){
 
-	}
-}
+
+
 }
