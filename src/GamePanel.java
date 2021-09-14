5062d398a25767cfdf91faee05d162ba24adc230
Aidan <Aidan@Aidan-GameDesktop.lan>
2021-09-13 19:57:06 -0700

    game control

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,18 +1,92 @@
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.Timer;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener, KeyListener{
+	Timer frameDraw;
+	Font titleFont;
+	Font startFont;
+	Font ruleFont;
+
 		    final int MENU = 0;
 		    final int GAME = 1;
 		    final int END = 2;
 	    int currentState = MENU;
+	    GamePanel(){
+	        frameDraw = new Timer(1000/60,this);
+	        frameDraw.start();
+	    	titleFont = new Font("Arial", Font.PLAIN, 32);
+	    	startFont = new Font("Arial", Font.PLAIN, 18);
+	    	ruleFont = new Font("Arial", Font.PLAIN, 18);
+	    }
 		public void paintComponent(Graphics g){
-			//yguyt
-			Font titleFont = new Font("Arial", Font.PLAIN, 48);
-			titleFont = new Font("Arial", Font.PLAIN, 48
-					);
+			if(currentState == MENU){
+			    drawMenuState(g);
+			}else if(currentState == GAME){
+			    drawGameState(g);
+			}else if(currentState == END){
+			    drawEndState(g);
+			}
+	
+		}
+		void updateMenuState() {  }
+		void updateGameState() {  }
+		void updateEndState()  {  }
+		void drawMenuState(Graphics g) { 
+			g.setColor(Color.BLUE);
+			g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+			g.setFont(titleFont);
+			g.setColor(Color.YELLOW);
+			g.drawString("LEAGUE INVADERS", 100, 50);
+			g.setFont(startFont);
+			g.setColor(Color.YELLOW);
+			g.drawString("Press ENTER to start", 150, 200);
+			g.setFont(titleFont);
+			g.setColor(Color.YELLOW);
+			g.drawString("Press SPACE for instructions", 50, 400);
+		}
+		void drawGameState(Graphics g) {  }
+		void drawEndState(Graphics g)  {  }
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if(currentState == MENU){
+			    updateMenuState();
+			}else if(currentState == GAME){
+			    updateGameState();
+			}else if(currentState == END){
+			    updateEndState();
+			}
+			repaint();
+		}
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			if (e.getKeyCode()==KeyEvent.VK_ENTER) {
+			    if (currentState == END) {
+			        currentState = MENU;
+			    } else {
+			        currentState++;
+			    }
+			}   
+
+		}
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
 		}
 
 	}
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.Graphics;
 
 public class LeagueInvaders {
+	
 	public static void main(String[] args) {
 new LeagueInvaders().setup();
 
@@ -16,10 +17,10 @@ public static final int WIDTH = 500;
 public static final int HEIGHT = 800;
 LeagueInvaders(){
 	frame = new JFrame();
+	
 }
 void setup() {
-	//astfe
-
+	
     
 	GamePanel panel = new GamePanel();
 	frame.add(panel);
