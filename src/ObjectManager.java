bc3091df422eaea9e1d4549e19b96364c2d8afb4
Aidan <Aidan@Aidan-GameDesktop.lan>
2021-09-22 19:56:01 -0700

    I worked on the Object manager but got confused

---
+++ src/ObjectManager.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ObjectManager {
+Rocketship rocket;
+ObjectManager(Rocketship rocket){
+	this.rocket=rocket;
+	Projectile projectiles;
+	ArrayList<Projectile> projectile = new ArrayList<Projectile>();
+}
+	void addProjectile(Projectile projectile) {
+	
+	}
+}
